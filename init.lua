3c7ac993f08244a98a25a604f938922bd6bbd6f4
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-10-11 16:44:00 -0400

    lsp config

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("config.default")
 require("plugins.packer")
-
 require('lualine').setup()
 require'nvim-web-devicons'.get_icons()
+require'lspconfig'.pyright.setup{}
+require('plugins.lspconfig')
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -3,6 +3,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use 'neovim/nvim-lspconfig' 
   use 'nvim-tree/nvim-web-devicons'
   use {
     'nvim-lualine/lualine.nvim',
