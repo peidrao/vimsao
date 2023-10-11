aad6f655e17e09c7a4a48c89f76a66bdab4bb08c
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-10-11 14:21:39 -0400

    add lualine and packer

--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -1,6 +1,12 @@
 vim.cmd [[packadd packer.nvim]]
 
+
 return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true } 
+  }
 
 end)
-
