a7a167354530db5f7be974c095e7d14a5cb17af5
🗿 Pedro Fonseca <contatopedrorn@gmail.com>
2023-10-12 16:58:50 -0300

    new configurations

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 require("config.default")
+require('config.maps')
 require("plugins.packer")
-require('lualine').setup()
+require('lualine').setup{ options = 
+	{ theme  = 'gruvbox-material', },
+}
+
 require'nvim-web-devicons'.get_icons()
 require'lspconfig'.pyright.setup{}
 require('plugins.lspconfig')
+
+require('plugins.nvim-tree')
+
+vim.o.background = "dark" 
+vim.cmd([[colorscheme gruvbox]])
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -2,6 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 
 return require('packer').startup(function(use)
+  use { "ellisonleao/gruvbox.nvim" }
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig' 
   use 'nvim-tree/nvim-web-devicons'
@@ -9,5 +10,8 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true } 
   }
-
+  use {
+	'nvim-tree/nvim-tree.lua',
+	requires = {'nvim-tree/nvim-web-devicons',},
+}
 end)
