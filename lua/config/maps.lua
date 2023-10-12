9db49f18bf7d0997ec68a1490c4398d3e21a7d58
🗿 Pedro Fonseca <contatopedrorn@gmail.com>
2023-10-12 16:57:51 -0300

    add maps

---
+++ lua/config/maps.lua
@@ -0,0 +1,28 @@
+local set = vim.api.nvim_set_keymap
+local ns = { noremap = true, silent = true }
+
+set("n", ";", ":", { noremap = true })
+
+set("n", "<Leader><Space>", ":let @/=''<CR>", ns)
+
+-- Window
+set("n", "<C-j>", ":resize -2<CR>", ns)
+set("n", "<C-k>", ":resize +2<CR>", ns)
+set("n", "<C-h>", ":vertical resize -2<CR>", ns)
+set("n", "<C-l>", ":vertical resize +2<CR>", ns)
+set("n", "<Leader>h", ":<C-u>split<CR>", ns)
+set("n", "<Leader>v", ":<C-u>vsplit<CR>", ns)
+
+vim.api.nvim_set_keymap('n', '<M-Right>', '<CMD>BufferLineCycleNext<CR>', ns)
+vim.api.nvim_set_keymap('n', '<M-Left>', '<CMD>BufferLineCyclePrev<CR>', ns)
+vim.api.nvim_set_keymap('n', '<C-DEL>', '<CMD>BufferLinePickClose<CR>', ns)
+
+-- Moving
+set("n", "j", "gj", ns)
+set("n", "k", "gk", ns)
+
+
+set('n', "<C-b>", ':NvimTreeToggle<CR>', ns)
+set('n', "<C-m>", ':NvimTreeFocus<CR>', ns)
+set('n', "<C-t>", ':Telescope find_files<CR>', ns)
+
