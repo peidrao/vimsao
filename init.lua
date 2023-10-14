354983f4e77bd4739eb8ba2547d1b0f976372c63
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-10-13 22:12:08 -0400

    update init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,15 @@
 require("config.default")
 require('config.maps')
-require("plugins.packer")
-require('lualine').setup{ 
-  options = { theme  = 'gruvbox-material', },
-}
-require'nvim-web-devicons'.get_icons()
-require'lspconfig'.pyright.setup{}
 require('plugins.lspconfig')
 require('plugins.completions')
 require('plugins.nvim-tree')
-require("bufferline").setup{}
+require("plugins.packer")
 require('plugins.treesitter')
+
+require'nvim-web-devicons'.get_icons()
+require'lspconfig'.pyright.setup{}
+require("bufferline").setup{}
+require('lualine').setup{ 
+  options = { theme  = 'gruvbox-material', },
+}
+
