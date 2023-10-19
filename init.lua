fdd375066a4b83a3e2818656094a4c716f343768
🐍 Pedro Fonseca <contatopedrorn@gmail.com>
2023-10-18 21:37:44 -0400

    config init.lua

--- init.lua
+++ init.lua
@@ -1,19 +1,22 @@
+-- default settings
 require("config.default")
 require('config.maps')
+
+-- plugins
 require("plugins.packer")
 require('plugins.lspconfig')
 require('plugins.completions')
 require('plugins.nvim-tree')
 require('plugins.treesitter')
 
-require'nvim-web-devicons'.get_icons()
-require'lspconfig'.pyright.setup{}
+require('nvim-web-devicons').get_icons()
+require('lspconfig').pyright.setup{}
 require("bufferline").setup{}
 require('lualine').setup{ 
-  options = { theme  = 'moonfly', },
+  options = { theme  = 'modus-vivendi', },
 }
 require('gitblame').setup {
-    enabled = true,
+    enabled = false,
 }
 
 require('gitsigns').setup()
